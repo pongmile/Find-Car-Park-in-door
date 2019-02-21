f1b3d13af60302e6ce268e57f8bd4bd79d999a72
pongsakorn khemanitthathai <it58070087@office365.it.kmitl.ac.th>
2019-02-21 14:34:36 +0700

    Fix Noti

---
+++ app/src/main/java/com/pongmile/mycarpark/MyFirebaseInstanceIDService.java
@@ -0,0 +1,11 @@
+package com.pongmile.mycarpark;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by pongmile on 21-Feb-19.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+}
--- app/src/main/java/com/pongmile/mycarpark/MyMessagingService.java
+++ app/src/main/java/com/pongmile/mycarpark/MyMessagingService.java
@@ -1,10 +1,18 @@
 package com.pongmile.mycarpark;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 /**
  * Created by pongmile on 21-Feb-19.
  */
 
 public class MyMessagingService extends FirebaseMessagingService {
+
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+    }
 }
